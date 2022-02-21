f7636dce2145066fdc15b4d51a5995ebefbb9bcb
Mikko Rautiainen <m_rautiainen@hotmail.com>
2022-02-20 19:31:55 -0500

    optimization

--- src/MEMfinder.h
+++ src/MEMfinder.h
@@ -24,6 +24,8 @@ namespace MEMfinder
 		size_t seqPos;
 		size_t matchLen;
 		template <typename F>
+		friend void iterateMEMGroupsInternal(const FMIndex&, const std::string&, const size_t, size_t, size_t, size_t, size_t, size_t, size_t, F);
+		template <typename F>
 		friend void iterateMEMGroups(const FMIndex&, const std::string&, const size_t, F);
 		template <typename F>
 		friend void iterateMEMs(const FMIndex&, const std::string&, const MatchGroup&, F);
@@ -32,72 +34,98 @@ namespace MEMfinder
 	uint8_t mapChar(const char);
 
 	template <typename F>
+	void iterateMEMGroupsInternal(const FMIndex& index, const std::string& seq, const size_t minLen, size_t lowStart, size_t lowEnd, size_t highStart, size_t highEnd, size_t end, size_t length, F callback)
+	{
+		while (length <= end)
+		{
+			size_t oldSize = (lowEnd - lowStart) + (highEnd - highStart);
+			if (oldSize == 0) break;
+			uint8_t c = mapChar(seq[end-length]);
+			size_t newLowStart = 0, newLowEnd = 0, newHighStart = 0, newHighEnd = 0;
+			if (lowEnd > lowStart)
+			{
+				newLowStart = index.advance(lowStart, c);
+				newLowEnd = index.advance(lowEnd, c);
+			}
+			if (highEnd > highStart)
+			{
+				newHighStart = index.advance(highStart, c);
+				newHighEnd = index.advance(highEnd, c);
+			}
+			size_t newSize = (newLowEnd - newLowStart) + (newHighEnd - newHighStart);
+			assert(newSize <= oldSize);
+			if (length >= minLen && newSize < oldSize)
+			{
+				callback(MatchGroup { oldSize - newSize, lowStart, lowEnd, highStart, highEnd, end - length + 1, length });
+			}
+			lowStart = newLowStart;
+			lowEnd = newLowEnd;
+			highStart = newHighStart;
+			highEnd = newHighEnd;
+			assert(lowEnd >= lowStart);
+			assert(highEnd >= highStart);
+			if (length == end)
+			{
+				callback(MatchGroup { newSize, lowStart, lowEnd, highStart, highEnd, end - length, length+1 });
+			}
+			length += 1;
+			if (newSize == 0) break;
+		}
+	}
+
+	template <typename F>
 	void iterateMEMGroups(const FMIndex& index, const std::string& seq, const size_t minLen, F callback)
 	{
+		assert(minLen >= 2);
+		size_t lastLow = 0;
+		size_t lastHigh = 0;
 		for (size_t end = seq.size()-1; end >= minLen-1; end--)
 		{
-			if (end != seq.size()-1 && mapChar(seq[end]) == 0) continue;
-			size_t lowStart;
-			size_t lowEnd;
-			size_t highStart;
-			size_t highEnd;
-			if (end == seq.size()-1 || mapChar(seq[end+1]) == 0)
+			if (mapChar(seq[end]) == 0)
 			{
-				lowStart = 0;
-				lowEnd = index.size();
-				highStart = 0;
-				highEnd = 0;
+				lastLow = 0;
+				lastHigh = 0;
+				continue;
 			}
-			else if (mapChar(seq[end+1]) == 5)
+			size_t low = 0;
+			size_t high = index.size();
+			for (size_t i = 0; i < minLen-1; i++)
 			{
-				lowStart = 0;
-				lowEnd = index.charStart(mapChar(seq[end+1]));
-				highStart = 0;
-				highEnd = 0;
+				size_t c = mapChar(seq[end-i]);
+				low = index.advance(low, c);
+				high = index.advance(high, c);
+				if (high == low)
+				{
+					lastLow = 0;
+					lastHigh = 0;
+					break;
+				}
 			}
-			else
+			if (high == low)
 			{
-				lowStart = 0;
-				lowEnd = index.charStart(mapChar(seq[end+1]));
-				highStart = index.charStart(mapChar(seq[end+1])+1);
-				highEnd = index.size();
+				lastLow = 0;
+				lastHigh = 0;
+				continue;
 			}
-			size_t length = 0;
-			while (length <= end)
+			if (lastLow != lastHigh)
 			{
-				size_t oldSize = (lowEnd - lowStart) + (highEnd - highStart);
-				if (oldSize == 0) break;
-				uint8_t c = mapChar(seq[end-length]);
-				size_t newLowStart = 0, newLowEnd = 0, newHighStart = 0, newHighEnd = 0;
-				if (lowEnd > lowStart)
+				size_t c = mapChar(seq[end-(minLen-2)]);
+				lastLow = index.advance(lastLow, c);
+				lastHigh = index.advance(lastHigh, c);
+				if (lastLow != lastHigh)
 				{
-					newLowStart = index.advance(lowStart, c);
-					newLowEnd = index.advance(lowEnd, c);
+					assert(lastLow >= low);
+					assert(lastHigh <= high);
+					if (lastLow == low && lastHigh == high) continue;
+					iterateMEMGroupsInternal(index, seq, minLen, low, lastLow, lastHigh, high, end, minLen-1, callback);
 				}
-				if (highEnd > highStart)
-				{
-					newHighStart = index.advance(highStart, c);
-					newHighEnd = index.advance(highEnd, c);
-				}
-				size_t newSize = (newLowEnd - newLowStart) + (newHighEnd - newHighStart);
-				assert(newSize <= oldSize);
-				if (length >= minLen && newSize < oldSize)
-				{
-					callback(MatchGroup { oldSize - newSize, lowStart, lowEnd, highStart, highEnd, end - length + 1, length });
-				}
-				lowStart = newLowStart;
-				lowEnd = newLowEnd;
-				highStart = newHighStart;
-				highEnd = newHighEnd;
-				assert(lowEnd >= lowStart);
-				assert(highEnd >= highStart);
-				if (length == end)
-				{
-					callback(MatchGroup { newSize, lowStart, lowEnd, highStart, highEnd, end - length, length+1 });
-				}
-				length += 1;
-				if (newSize == 0) break;
 			}
+			if (lastLow == lastHigh)
+			{
+				iterateMEMGroupsInternal(index, seq, minLen, low, high, 0, 0, end, minLen-1, callback);
+			}
+			lastLow = low;
+			lastHigh = high;
 		}
 	}
 
